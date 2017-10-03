c1bf77041d7b69329556f24c11a10cff5a60ef1c
tobyweston <toby.weston@gmail.com>
2017-10-03 16:10:24 +0100

    some standard async nonsense

---
+++ src/main/scala/bad/robot/webdriver/Locator.scala
@@ -0,0 +1,34 @@
+package bad.robot.webdriver
+
+import org.openqa.selenium.{By, WebDriver, WebElement}
+import bad.robot.webdriver
+
+/**
+  * implicit version of the `bad.robot.webdriver` helper methods.
+  */
+object Locator {
+
+  implicit class LocatorOps(locator: By) {
+
+    def waitForElement(implicit driver: WebDriver): WebElement = {
+      webdriver.waitForElement(locator)
+    }
+
+    def findElements(implicit driver: WebDriver): List[WebElement] = {
+      webdriver.findElements(locator)
+    }
+
+    def waitForElementToBeInteractable(implicit driver: WebDriver): WebElement = {
+      webdriver.waitForElementToBeInteractable(locator)
+    }
+
+    def waitUntilVisible(implicit driver: WebDriver): WebElement = {
+      webdriver.waitUntilVisible(locator)
+    }
+
+    def waitUntilNotPresent(implicit driver: WebDriver): Unit = {
+      webdriver.waitUntilNotPresent(locator)
+    }
+  }
+
+}
---
+++ src/main/scala/bad/robot/webdriver/package.scala
@@ -0,0 +1,62 @@
+package bad.robot
+
+import com.google.common.base.{Function => GuavaFunction, Predicate => GuavaPredicate}
+import org.openqa.selenium._
+import org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable
+import org.openqa.selenium.support.ui.WebDriverWait
+
+import scala.collection.JavaConverters
+
+package object webdriver {
+
+  implicit def functionToPredicate(predicate: WebDriver => Boolean): GuavaPredicate[WebDriver] = {
+    (driver: WebDriver) => predicate.apply(driver)
+  }
+
+  implicit def functionToFunction[A, W >: WebDriver](function: W => A): GuavaFunction[W, A] = {
+    (driver: W) => function.apply(driver)
+  }
+
+  def waitForElement(locator: By)(implicit driver: WebDriver): WebElement = {
+    waitForCondition(driver, _.findElement(locator), s"waiting for element '$locator' on page '${driver.getCurrentUrl}'")
+  }
+
+  def findElements(locator: By)(implicit driver: WebDriver): List[WebElement] = {
+    import JavaConverters._
+    driver.findElements(locator).asScala.toList
+  }
+
+  def waitForElementToBeInteractable(locator: By)(implicit driver: WebDriver): WebElement = {
+    val interactable: (WebDriver) => Boolean = e => {
+      val element: WebElement = e.findElement(locator)
+      element.isEnabled && element.isDisplayed
+    }
+
+    waitForCondition(driver, interactable, s"waiting for element '$locator' on page '${driver.getCurrentUrl}'")
+    driver.findElement(locator)
+  }
+
+  def waitUntilVisible(locator: By)(implicit driver: WebDriver): WebElement = {
+    waitForCondition(driver, _.findElement(locator).isDisplayed, s"waiting for element $locator to be visible")
+    driver.findElement(locator)
+  }
+
+  def waitUntilClickable(element: WebElement)(implicit driver: WebDriver): WebElement = {
+    new WebDriverWait(driver, 5).until(elementToBeClickable(element))
+  }
+
+  def waitUntilNotPresent(locator: By)(implicit driver: WebDriver): Unit = {
+    waitForCondition(driver, _.findElements(locator).size() == 0, s"waiting for element $locator to not be present")
+  }
+
+  private def waitForCondition[A](driver: WebDriver, predicate: WebDriver => A, message: String): A = {
+    new WebDriverWait(driver, 5)
+      .withMessage(message)
+      .ignoring(classOf[NoSuchElementException])
+      .ignoring(classOf[ElementNotVisibleException])
+      .ignoring(classOf[StaleElementReferenceException])
+      .ignoring(classOf[NoAlertPresentException])
+      .until(predicate)
+  }
+
+}
