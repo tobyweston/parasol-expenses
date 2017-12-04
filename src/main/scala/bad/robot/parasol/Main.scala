68718415f5c7f268c190110973ad5c75109846d9
Toby <toby.weston@gmail.com>
2017-12-04 19:44:55 +0000

    add mac support

--- src/main/scala/bad/robot/parasol/Main.scala
+++ src/main/scala/bad/robot/parasol/Main.scala
@@ -2,9 +2,8 @@ package bad.robot.parasol
 
 import java.io.{File, FileNotFoundException}
 
-import bad.robot.parasol.site.ExpenseSummaryPredicates
 import bad.robot.parasol.site.ExpenseSummaryPredicates._
-import bad.robot.parasol.site.domain.{April2016, ExpenseSummary}
+import bad.robot.parasol.site.domain.April2016
 import bad.robot.parasol.site.page.{ExpenseClaimPage, LandingPage}
 import org.openqa.selenium.chrome.{ChromeDriver, ChromeOptions}
 
@@ -45,10 +44,14 @@ object Main extends App {
   }
 
   private def setDriver() = {
-    val os: Option[String] = sys.props.get("os.name")
-    println(os)
-    val executable = s"src/main/resources/chromedriver_win32_2.31/chromedriver.exe"
-
+    val os = sys.props.get("os.name")
+    
+    val executable = os match {
+      case Some("Mac OS X")                                    => "src/main/resources/chromedriver_mac64_2.31/chromedriver"
+      case Some(other) if other.toLowerCase contains "windows" => "src/main/resources/chromedriver_win32_2.31/chromedriver.exe"
+      case None                                                => ???
+    }
+    
     if (!new File(executable).exists())
       throw new FileNotFoundException(s"Can't find driver based on the working directory: $executable")
 
